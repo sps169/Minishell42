729b25d03429428aa14dbea6bbcb65a1f5c7b389
Miguel <mglezg8@gmail.com>
2025-03-25 07:32:46 +0000

    Builting error fixed

--- src/builtins/builtins_dist.c
+++ src/builtins/builtins_dist.c
@@ -7,19 +7,19 @@
 
 int built_comprobation(t_tools *tools)
 {
-	if (!ft_strncmp(tools->command->args[0], "echo", 4))
+	if (!ft_strcmp(tools->command->args[0], "echo"))
 		return (1);
-	if (!ft_strncmp(tools->command->args[0], "cd", 2))
+	if (!ft_strcmp(tools->command->args[0], "cd"))
 		return (1);
-	if (!ft_strncmp(tools->command->args[0], "pwd", 3))
+	if (!ft_strcmp(tools->command->args[0], "pwd"))
 		return (1);
-	if (!ft_strncmp(tools->command->args[0], "export", 6))
+	if (!ft_strcmp(tools->command->args[0], "export"))
 		return (1);
-	if (!ft_strncmp(tools->command->args[0], "unset", 4))
+	if (!ft_strcmp(tools->command->args[0], "unset"))
 		return (1);
-	if (!ft_strncmp(tools->command->args[0], "env", 3))
+	if (!ft_strcmp(tools->command->args[0], "env"))
 		return (1);
-	if (!ft_strncmp(tools->command->args[0], "exit", 4))
+	if (!ft_strcmp(tools->command->args[0], "exit"))
 		return (1);
 	else 
 		return (0);
@@ -28,19 +28,19 @@ int built_comprobation(t_tools *tools)
 
 void ft_builtin(t_tools *tools)
 {
-	if (!ft_strncmp(tools->command->args[0], "env", 3))
+	if (!ft_strcmp(tools->command->args[0], "env"))
 		ft_env(tools);
-	if (!ft_strncmp(tools->command->args[0], "pwd", 3))
+	if (!ft_strcmp(tools->command->args[0], "pwd"))
 		ft_pwd(tools);
-	if (!ft_strncmp(tools->command->args[0], "echo", 4))
+	if (!ft_strcmp(tools->command->args[0], "echo"))
 		ft_echo(tools);
-	if (!ft_strncmp(tools->command->args[0], "exit", 4))
+	if (!ft_strcmp(tools->command->args[0], "exit"))
 		ft_exit(tools);
-	if (!ft_strncmp(tools->command->args[0], "export", 6))
+	if (!ft_strcmp(tools->command->args[0], "export"))
 		ft_export(tools);
-	if (!ft_strncmp(tools->command->args[0], "unset", 4))
+	if (!ft_strcmp(tools->command->args[0], "unset"))
 		ft_unset(tools);
-	 if (!ft_strncmp(tools->command->args[0], "cd", 2))
+	 if (!ft_strcmp(tools->command->args[0], "cd"))
 	 	ft_cd(tools);
 }
 
